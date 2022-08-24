6a5b8f44a5d09a7a1369c0aa887bf7ce482b8798
Kade Fullmer <fullmer24@gmail.com>
2022-08-24 09:09:19 -0600

    updated env

--- Tower.client/src/env.js
+++ Tower.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = 'mine24.us.auth0.com'
-export const clientId = 'OvisIjf9rdBOs2TNUn2uHIpLSdtHmulG'
-export const audience = 'https://codeworksclassroom.com'
+export const domain = 'dev-n068hm4t.us.auth0.com'
+export const clientId = 'stTTmMW6eCuAE0NzAkk8OLn2IGiWqInr'
+export const audience = 'https://hackathon.com'
