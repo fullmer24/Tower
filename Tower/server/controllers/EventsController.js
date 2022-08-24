4490c069948eff8f6980464f3b4085376de4b2c9
Kade Fullmer <fullmer24@gmail.com>
2022-08-24 12:21:01 -0600

    get by id and edit working and passed

--- Tower/server/controllers/EventsController.js
+++ Tower/server/controllers/EventsController.js
@@ -6,8 +6,19 @@ export class EventsController extends BaseController {
     constructor() {
         super('api/events')
         this.router
+            .get('', this.getAll)
+            .get('/:id', this.getById)
             .use(Auth0Provider.getAuthorizedUserInfo)
             .post('', this.create)
+            .put('/:eventId', this.editEvent)
+    }
+    async getById(req, res, next) {
+        try {
+            const event = await eventsService.getById(req.params.id)
+            return res.send(event)
+        } catch (error) {
+            next(error)
+        }
     }
 
     async create(req, res, next) {
@@ -20,7 +31,26 @@ export class EventsController extends BaseController {
         }
     }
 
+    async getAll(req, res, next) {
+        try {
+            const events = await eventsService.getAll()
+            return res.send(events)
+        } catch (error) {
+            next.error(error)
+        }
+    }
 
+    async editEvent(req, res, next) {
+        try {
+            req.body.creatorId = req.userInfo.id
+            let eventData = req.body
+            let event = await eventsService.editEvent(req.params.eventId, eventData)
+            res.send(event)
+        }
+        catch (error) {
+            next(error)
+        }
+    }
 
 
 }
