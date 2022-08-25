499a7d81641fb1d1a2b247fb34b00f1d82660c75
Kade Fullmer <fullmer24@gmail.com>
2022-08-25 11:32:08 -0600

    Comment db and Schema written

--- Tower/server/db/DbContext.js
+++ Tower/server/db/DbContext.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
+import { CommentSchema } from '../models/Comments.js';
 import { EventSchema } from '../models/Event.js';
 import { TicketSchema } from '../models/Tickets.js';
 import { ValueSchema } from '../models/Value'
@@ -8,8 +9,9 @@ class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
   Events = mongoose.model('Event', EventSchema);
-
   Tickets = mongoose.model('Ticket', TicketSchema);
+
+  Comments = mongoose.model('Comment', CommentSchema)
 }
 
 export const dbContext = new DbContext()
---
+++ Tower/server/models/Comments.js
@@ -0,0 +1,18 @@
+import { Schema } from "mongoose";
+const ObjectId = Schema.Types.ObjectId
+
+export const CommentSchema = new Schema({
+    creatorId: { type: ObjectId, required: true },
+    eventId: { type: ObjectId, required: true },
+    body: { type: String, required: true },
+    isAttending: { type: Boolean, required: true }
+}, {
+    timestamps: true, toJSON: { virtuals: true }
+})
+
+CommentSchema.virtual('creator', {
+    localField: 'creatorId',
+    foreignField: '_id',
+    ref: 'Account',
+    justOne: true
+})
